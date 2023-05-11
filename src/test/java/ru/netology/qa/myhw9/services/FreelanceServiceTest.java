6e6d0a155bba78109b19e4ec084aaf2befd73637
Olga Oblat <oa.oblat@gmail.com>
2023-05-11 14:20:00 +0300

    added ParameterizedTest

--- src/test/java/ru/netology/qa/myhw9/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/qa/myhw9/services/FreelanceServiceTest.java
@@ -2,15 +2,18 @@ package ru.netology.qa.myhw9.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FreelanceServiceTest {
 
-    @Test
-    public void shouldCalcMonth() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/freelance.csv")
+
+    public void shouldCalcMonth(int expected, int income, int expences, int threshold) {
         FreelanceService service = new FreelanceService();
 
-        int expected = 2;
-        int actual = service.calcMonthOfRest(100_000, 60_000, 150_000);
+        int actual = service.calcMonthOfRest(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
