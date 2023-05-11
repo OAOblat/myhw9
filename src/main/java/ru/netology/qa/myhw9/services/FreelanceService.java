4ed9ba2708fda854453a8cf6b0cf2f432c2d1031
Olga Oblat <oa.oblat@gmail.com>
2023-05-11 13:44:25 +0300

    changed src/

--- src/main/java/ru/netology/qa/myhw9/services/FreelanceService.java
+++ src/main/java/ru/netology/qa/myhw9/services/FreelanceService.java
@@ -8,7 +8,7 @@ public class FreelanceService {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money  - expenses;
+                money = money - expenses;
                 money = money / 3;
             } else {
                 money = money + income - expenses;
--- src/test/java/ru/netology/qa/myhw9/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/qa/myhw9/services/FreelanceServiceTest.java
@@ -3,17 +3,17 @@ package ru.netology.qa.myhw9.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-    public class FreelanceServiceTest {
+public class FreelanceServiceTest {
 
-     @Test
-        public void shouldCalcExact() {
-            FreelanceService service = new FreelanceService();
+    @Test
+    public void shouldCalcMonth() {
+        FreelanceService service = new FreelanceService();
 
-            int expected = 3;
-            int actual = service.calcMonthOfRest(100_000,60_000, 150_000);
+        int expected = 2;
+        int actual = service.calcMonthOfRest(100_000, 60_000, 150_000);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
 
 }
